ff105c16a9fe5e26804ac12ae4884e696c98e1f4
khainacs <khai.nguyenanh03@gmail.com>
2024-10-05 16:05:02 +0700

    feat: create layouts and formLogin

---
+++ website/src/pages/Login/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+import FormLogin from './components/FormLogin';
+
+
+Login.propTypes = {
+    title: PropTypes.string,
+};
+export default function Login(props) {
+    const { title } = props;
+
+    useEffect(() => {
+        document.title = title ? `${title}` : "Không tìm thấy trang";
+    }, [title]);
+
+    return (
+        <main className="justify-center d-flex">
+            <FormLogin />
+        </main>
+    );
+}
