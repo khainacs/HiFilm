5d51f304bface79c230695ecc0f730ca606467df
HNMPhuoc <143460139+HNMPhuoc@users.noreply.github.com>
2024-11-18 13:38:29 +0700

    feat: add handler oauth2 success and failure

---
+++ backend/Security/src/main/java/com/avg/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,99 @@
+package com.avg.security.handler;
+
+import com.avg.security.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.avg.security.entities.CustomUserDetail;
+import com.avg.security.entities.User;
+import com.avg.security.service.TokenService;
+import com.avg.security.service.impl.JwtService;
+import com.avg.security.utils.CookieUtils;
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+import static com.avg.security.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+@Component
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+    private final JwtService jwtService;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+    private final TokenService tokenService;
+
+    @Autowired
+    public OAuth2AuthenticationSuccessHandler(JwtService jwtService,
+                                              HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository, TokenService tokenService) {
+        this.jwtService = jwtService;
+        this.httpCookieOAuth2AuthorizationRequestRepository = httpCookieOAuth2AuthorizationRequestRepository;
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
+
+        if (response.isCommitted()) {
+            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            return;
+        }
+
+        clearAuthenticationAttributes(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue);
+
+        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+            try {
+                throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+            } catch (BadRequestException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+        CustomUserDetail customUserDetail = (CustomUserDetail) authentication.getPrincipal();
+        User user = customUserDetail.getUser();
+
+        // Generate token with rememberMe set to true
+        var jwtToken = jwtService.generateToken(customUserDetail, true);
+        var refreshToken = jwtService.generateRefreshToken(user);
+
+        // Save the token and revoke old ones
+        tokenService.revokedAllUserTokens(user);
+        tokenService.saveUserToken(user, jwtToken);
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", jwtToken)
+                .build().toUriString();
+    }
+
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        Dotenv dotenv = Dotenv.load();
+        String AUTHORIZED_REDIRECT_URI = dotenv.get("AUTHORIZED_REDIRECT_URI");
+        URI clientRedirectUri = URI.create(uri);
+
+        URI authorizedURI = URI.create(AUTHORIZED_REDIRECT_URI);
+        return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                && authorizedURI.getPort() == clientRedirectUri.getPort();
+    }
+}
